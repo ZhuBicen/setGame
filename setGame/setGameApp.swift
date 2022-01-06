9d041d59a8e84417ee1d6d75954f7c81a0e3f9d2
Zhu Bicen <zhubicen@gmail.com>
2022-01-06 20:59:29 +0800

    Initial Commit

---
+++ setGame/setGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  setGameApp.swift
+//  setGame
+//
+//  Created by Bicen Zhu on 2022/1/6.
+//
+
+import SwiftUI
+
+@main
+struct setGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
